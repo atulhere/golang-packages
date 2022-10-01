ea83f88fe802c1dc9f5f0e5bf6a76697317e252f
Atul Kumar Singh <atulsolanki30@gmail.com>
2022-10-01 21:23:26 +0530

    Use of packages

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"golang-packages/sum"
+)
+
+func main() {
+
+	fmt.Println("Sum of two numbers ", sum.Sum(19, 31))
+}
---
+++ sum/sum.go
@@ -0,0 +1,6 @@
+package sum
+
+func Sum(a int, b int) int {
+
+	return a + b
+}
